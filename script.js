5bfadd014df4c216fad61f206b8e516518003eb5
Behruz <Behruz>
2024-08-28 14:52:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var name = prompt("Ismingizni kiriting")
+
+console.log("Sizning ismingiz: " + name)
+
+var age = prompt("Yoshingizni kiriting")
+
+console.log("Sizning yoshingiz: " + age)
+
+var misol = prompt("6+3=?")
+
+  console.log("6+3=" + misol)
+
+var misol = prompt("25-19=?")
+
+console.log("25-19=" + misol)
+
+var misol = prompt("9*3=?")
+
+console.log("9*3=" + misol)
+
+var misol = prompt("21/3=?")
+
+console.log("21/3=" + misol)
+
+alert('Javoblar hammasi consoleda')
